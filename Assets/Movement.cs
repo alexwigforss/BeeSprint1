37db36b0d471bb85618dbfb0a72c33a09eca0971
Alex Wigforss <alex.wigforss@gmail.com>
2024-09-10 12:02:04 +0200

    Acceleration started

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,56 +8,89 @@ using UnityEngine.Windows;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody rb;
-    [SerializeField] float moveSpeed;
-    Vector3 moveForce = Vector3.zero;
+	Rigidbody rb;
+	[SerializeField] float acceleration;
+	[SerializeField] float moveSpeed;
+	Vector3 moveForce = Vector3.zero;
 	Vector3 rotateForce = Vector3.zero;
+	Boolean accelerate = false;
+	Boolean reverse = false;
+	Boolean slowdown = false;
 	void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-    private void Update()
-    {
-        if (rb != null)
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	private void Update()
+	{
+		if (rb != null)
+		{
+			Vector3 worldMoveForce = rb.transform.TransformDirection(moveForce);
+			rb.velocity += worldMoveForce;
+		}
+
+        if (accelerate && moveSpeed < 10.0f)
         {
-            Vector3 worldMoveForce = rb.transform.TransformDirection(moveForce);
-            rb.velocity += worldMoveForce;
-            //rb.transform.Rotate(rotateForce);
+			moveSpeed += acceleration;
+			moveForce.z = moveSpeed;
         }
-    }
+        else if (!accelerate && moveSpeed > 0.0f)
+        {
+			moveSpeed -= acceleration;
+			moveForce.z = moveSpeed;
+		}
+        //else
+        //{
+        //    moveForce.z = 0.0f;
+        //}
 
-    public void HandleX()
-    {
-        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
-        rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(0f, mouseDelta.x/10, 0f));
-    }
-    public void HandleY()
-    {
-        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
-        rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(mouseDelta.y / 10, 0f, 0f));
-    }
+		if (reverse && moveSpeed > -10.0f)
+			{
+				moveSpeed -= acceleration;
+				moveForce.z = moveSpeed;
+			}
+			else if (!reverse && moveSpeed < 0.0f)
+			{
+				moveSpeed += acceleration;
+				moveForce.z = moveSpeed;
+			}
+			//else
+			//{
+			//	moveForce.z = 0.0f;
+			//}
+		}
 
-    public void HandleMoveUp(InputAction.CallbackContext context)
-    {
-        if (context.performed) { moveForce.y = moveSpeed; }
-        else if (context.canceled) { moveForce.y = 0; }
-    }
-    public void HandleMoveDown(InputAction.CallbackContext context)
-    {
-        if (context.performed) { moveForce.y = -moveSpeed; }
-        else if (context.canceled) { moveForce.y = 0; }
-    }
+	public void HandleX()
+	{
+		Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+		rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(0f, mouseDelta.x / 10, 0f));
+	}
+	public void HandleY()
+	{
+		Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+		rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + new Vector3(mouseDelta.y / 10, 0f, 0f));
+	}
 
-    public void HandleMoveRight(InputAction.CallbackContext context)
-    {
-        //if (context.performed) { rotateForce.y += moveSpeed; }
+	public void HandleMoveUp(InputAction.CallbackContext context)
+	{
+		if (context.performed) { moveForce.y = moveSpeed; }
+		else if (context.canceled) { moveForce.y = 0; }
+	}
+	public void HandleMoveDown(InputAction.CallbackContext context)
+	{
+		if (context.performed) { moveForce.y = -moveSpeed; }
+		else if (context.canceled) { moveForce.y = 0; }
+	}
+
+	public void HandleMoveRight(InputAction.CallbackContext context)
+	{
+		//if (context.performed) { rotateForce.y += moveSpeed; }
 		//else if (context.canceled) { rotateForce.y = 0; }
 		if (context.performed) { moveForce.x = moveSpeed; }
 		else if (context.canceled) { moveForce.x = 0; }
 	}
 
-    public void HandleMoveLeft(InputAction.CallbackContext context)
-    {
+	public void HandleMoveLeft(InputAction.CallbackContext context)
+	{
 		//if (context.performed) { rotateForce.y -= moveSpeed; }
 		//else if (context.canceled) { rotateForce.y = 0; }
 		if (context.performed) { moveForce.x = -moveSpeed; }
@@ -65,13 +98,29 @@ public class Movement : MonoBehaviour
 	}
 
 	public void HandleMoveForward(InputAction.CallbackContext context)
-    {
-        if (context.performed) { moveForce.z = moveSpeed * 2; }
-        else if (context.canceled) { moveForce.z = 0; }
+	{
+		//moveSpeed += acceleration;
+		if (context.performed)
+		{
+			accelerate = true;
+		}
+        else if (context.canceled)
+        {
+			accelerate = false;
+        }
     }
-    public void HandleMoveBackward(InputAction.CallbackContext context)
-    {
-        if (context.performed) { moveForce.z = -moveSpeed * 2; }
-        else if (context.canceled) { moveForce.z = 0; }
-    }
+	public void HandleMoveBackward(InputAction.CallbackContext context)
+	{
+		//moveSpeed += acceleration;
+		if (context.performed)
+		{
+			reverse = true;
+		}
+		else if (context.canceled)
+		{
+			reverse = false;
+		}
+		//if (context.performed) { moveForce.z = -moveSpeed * 2; }
+		//else if (context.canceled) { moveForce.z = 0; }
+	}
 }
