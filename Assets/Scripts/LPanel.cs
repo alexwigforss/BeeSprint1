807ce8836e9482ab962e3ba07c7ee401b6f80ae3
Alex Wigforss <alex.wigforss@gmail.com>
2024-11-07 17:26:08 +0100

    Show amount in drone group
    If drones are in a group a label tells how many drones are in the group

--- Assets/Scripts/LPanel.cs
+++ Assets/Scripts/LPanel.cs
@@ -11,41 +11,63 @@ public class LPanel : Menu
 	protected override List<Sprite> GetSprites()
 	{
 		List<Sprite> sprites = new List<Sprite>();
-
-		foreach (Transform bee in spawnersParent)
+		TextMeshProUGUI tmp = new TextMeshProUGUI();
+		Texture2D texture = icon;
+		if (texture != null)
 		{
-			if (bee != null)
+			Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+			foreach (Transform item in spawnersParent)
 			{
-				Debug.Log("BAMMM BEE");
-				Texture2D texture = icon;
-
-				if (texture != null)
+				if (item != null)
 				{
-					Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-					sprites.Add(sprite);
+					if (item.name.Contains("Bee"))
+					{
+						GameObject spriteObject = getBeeIcon(sprite, item);
+						tmp = getIconText(spriteObject);
+						spritesadded++;
 
-					// Create a new GameObject for the sprite
-					GameObject spriteObject = new GameObject("SpriteObject");
-					spriteObject.transform.SetParent(layoutGroup, false); // Set false to keep local scale and position
+					}
+					else if(item.name.Contains("Group"))
+					{
+						int noOfBeesInGroup = 0;
+						GameObject spriteObject = getBeeIcon(sprite, item);
+						tmp = getIconText(spriteObject);
+						foreach (Transform subitem in item)
+						{
+							noOfBeesInGroup++;
+						}
+						tmp.text = noOfBeesInGroup.ToString();
+					}
 
-					// Add an Image component to the GameObject
-					Image image = spriteObject.AddComponent<Image>();
-					image.sprite = sprite;
-
-					// Create a new GameObject for the TextMeshPro
-					GameObject textObject = new GameObject("TextMeshProObject");
-					textObject.transform.SetParent(spriteObject.transform, false); // Set false to keep local scale and position
-					textObject.transform.localPosition = Vector3.zero; // Adjust the position as needed
-					
-					// Add a TextMeshPro component to the GameObject
-					TextMeshProUGUI tmp = textObject.AddComponent<TextMeshProUGUI>();
-					tmp.text = "0"; // Set your desired label text here
-					tmp.alignment = TextAlignmentOptions.Center;
-					spritesadded++;
-				}
+                }
 			}
 		}
-
 		return sprites;
+	}
+
+	private static TextMeshProUGUI getIconText(GameObject spriteObject)
+	{
+		// Create a new GameObject for the TextMeshPro
+		GameObject textObject = new GameObject("TextMeshProObject");
+		textObject.transform.SetParent(spriteObject.transform, false); // Set false to keep local scale and position
+		textObject.transform.localPosition = Vector3.zero; // Adjust the position as needed
+
+		// Add a TextMeshPro component to the GameObject
+		TextMeshProUGUI tmp = textObject.AddComponent<TextMeshProUGUI>();
+		tmp.alignment = TextAlignmentOptions.Center;
+		tmp.text = "";
+		return tmp;
+	}
+
+	private GameObject getBeeIcon(Sprite sprite, Transform item)
+	{
+		// Create a new GameObject for the sprite
+		GameObject spriteObject = new GameObject("SpriteObject");
+		spriteObject.transform.SetParent(layoutGroup, false); // Set false to keep local scale and position
+
+		// Add an Image component to the GameObject
+		Image image = spriteObject.AddComponent<Image>();
+		image.sprite = sprite;
+		return spriteObject;
 	}
 }
