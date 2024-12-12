62fce9a15fb426871f4831bf28b53ed3a8c824ec
Alex Wigforss <alex.wigforss@gmail.com>
2024-12-12 11:44:26 +0100

    SelectiveMemory
    To store and handle which species to collect from

---
+++ Assets/Scripts/SelectiveMemory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectiveMemory : MonoBehaviour
+{
+    List<int> memory = new List<int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
