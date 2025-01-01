b868c7258a146c91abbcad6e592f10610eac97ba
Alex Wigforss <alex.wigforss@gmail.com>
2025-01-01 14:40:58 +0100

    LOD on wing rotation

--- Assets/Scripts/Flapper.cs
+++ Assets/Scripts/Flapper.cs
@@ -4,12 +4,19 @@ public class Flapper : MonoBehaviour {
 	private bool upwards = false;
 	private int noOfSteps = 3;
 	private int step;
+	private Transform mainCamera;
+	[SerializeField] private float lodDistance = 20.0f;
 
 	private void Start() {
 		step = noOfSteps;
+		mainCamera = Camera.main.transform;
 	}
 
 	void Update() {
+		if (Vector3.Distance(transform.position, mainCamera.position) > lodDistance) {
+			return;
+		}
+
 		if (!upwards) {
 			if (step-- > 0) {
 				transform.Rotate(new Vector3(0f, 0f, -1000f) * Time.deltaTime);
