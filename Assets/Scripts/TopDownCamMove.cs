9155cc12bc1be1a10f648899c4ba992c0daaee7b
Alex Wigforss <alex.wigforss@gmail.com>
2025-01-02 16:07:47 +0100

    Tilted the Top Down Camera
    In order to preserve movement behavior i added a parent object to the camera which the script move instead of the camera itself.

--- Assets/Scripts/TopDownCamMove.cs
+++ Assets/Scripts/TopDownCamMove.cs
@@ -7,60 +7,73 @@ using UnityEngine.UIElements;
 using UnityEngine.Windows;
 
 public class TopDownCamMove : MonoBehaviour {
-	Rigidbody rb;
-	Camera cam;
 	[SerializeField] float moveSpeed;
 	[SerializeField] float rotoSpeed;
 	Vector3 moveForce = Vector3.zero;
 	float rotoForce = 0.0f;
+
 	void Start() {
-		cam = GetComponent<Camera>();
-		cam.transform.position = new Vector3(0, 45, 0);
+		// Set the initial position of the parent GameObject
+		transform.parent.position = new Vector3(0, 45, 0);
 	}
+
 	private void Update() {
-		if (cam != null) {
-			Vector3 worldMoveForce = cam.transform.TransformDirection(moveForce) * Time.deltaTime;
-			Vector3 worldRotateForce = new Vector3(0, 0, rotoForce) * Time.deltaTime;
-			cam.transform.position += worldMoveForce;
-			cam.transform.Rotate(worldRotateForce);
+		// Apply movement in the parent's local space for x and z axes
+		Vector3 localMoveForce = new Vector3(moveForce.x, 0, moveForce.z) * Time.deltaTime;
+		transform.parent.Translate(localMoveForce, Space.Self);
+
+		// Apply movement in world space for y-axis (zoom)
+		Vector3 worldMoveForce = new Vector3(0, moveForce.y, 0) * Time.deltaTime;
+		transform.parent.position += worldMoveForce;
+
+		// Apply rotation around the parent's local y-axis
+		if (rotoForce != 0) {
+			transform.parent.Rotate(0, rotoForce * Time.deltaTime, 0, Space.Self);
 		}
 	}
+
 	public void HandleCamLeft(InputAction.CallbackContext context) {
 		if (context.performed) {
 			moveForce.x = -moveSpeed;
 		} else if (context.canceled) { moveForce.x = 0; }
-
 	}
+
 	public void HandleCamRight(InputAction.CallbackContext context) {
 		if (context.performed) {
 			moveForce.x = moveSpeed;
 		} else if (context.canceled) { moveForce.x = 0; }
 	}
+
 	public void HandleCamUp(InputAction.CallbackContext context) {
-		if (context.performed) {
-			moveForce.y = moveSpeed;
-		} else if (context.canceled) { moveForce.y = 0; }
-	}
-	public void HandleCamDown(InputAction.CallbackContext context) {
-		if (context.performed) {
-			moveForce.y = -moveSpeed;
-		} else if (context.canceled) { moveForce.y = 0; }
-	}
-	public void HandleZoomIn(InputAction.CallbackContext context) {
 		if (context.performed) {
 			moveForce.z = moveSpeed;
 		} else if (context.canceled) { moveForce.z = 0; }
 	}
-	public void HandleZoomOut(InputAction.CallbackContext context) {
+
+	public void HandleCamDown(InputAction.CallbackContext context) {
 		if (context.performed) {
 			moveForce.z = -moveSpeed;
 		} else if (context.canceled) { moveForce.z = 0; }
 	}
+
+	public void HandleZoomIn(InputAction.CallbackContext context) {
+		if (context.performed) {
+			moveForce.y = -moveSpeed; // Move down in world space
+		} else if (context.canceled) { moveForce.y = 0; }
+	}
+
+	public void HandleZoomOut(InputAction.CallbackContext context) {
+		if (context.performed) {
+			moveForce.y = moveSpeed; // Move up in world space
+		} else if (context.canceled) { moveForce.y = 0; }
+	}
+
 	public void HandleCamRotateCounterClock(InputAction.CallbackContext context) {
 		if (context.performed) {
 			rotoForce = -rotoSpeed;
 		} else if (context.canceled) { rotoForce = 0; }
 	}
+
 	public void HandleCamRotateClockwise(InputAction.CallbackContext context) {
 		if (context.performed) {
 			rotoForce = rotoSpeed;
